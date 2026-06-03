39aea9bfc8326cf9bca6145c87f4fbdaa232bf60
rajasekhargeddam <ganeshrajasekhar219@gmail.com>
2026-06-03 22:21:39 +0530

    responsiveness

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -7,7 +7,7 @@ type CardListProps = {
 
 const CardList = ({ posts }: CardListProps) => {
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 p-4 py-8 mx-auto max-w-6xl list-none">
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 py-8 mx-auto max-w-6xl list-none">
       {posts.map((post) => (
         <Card key={post.id} post={post} />
       ))}
--- src/shimmerUi/ShimmerPosts.tsx
+++ src/shimmerUi/ShimmerPosts.tsx
@@ -6,7 +6,7 @@ type ShimmerPostsProps = {
 
 const ShimmerPosts = ({ count = 12 }: ShimmerPostsProps) => {
   return (
-    <ul className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 p-4 py-8 mx-auto max-w-6xl list-none">
+    <ul className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 py-8 mx-auto max-w-6xl list-none">
       {Array.from({ length: count }).map((_, index) => (
         <ShimmerCard key={index} />
       ))}

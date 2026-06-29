20a0818b18888292859f5a166a98e338f77694bf
rajasekhargeddam <ganeshrajasekhar219@gmail.com>
2026-06-29 16:45:46 +0530

    refactor: update API URLs to use production base URL

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,9 +2,19 @@ export const POSTS_API_URL = "https://jsonplaceholder.typicode.com/posts";
 export const POSTS_PER_PAGE = 12;
 
 // auth APIs
-export const LOGIN_API = "http://localhost:3000/login";
-export const SIGNUP_API = "http://localhost:3000/signup";
-export const LOGOUT_API = "http://localhost:3000/logout";
+// export const LOGIN_API = "http://localhost:3000/login";
+// export const SIGNUP_API = "http://localhost:3000/signup";
+// export const LOGOUT_API = "http://localhost:3000/logout";
+
+// // Profile
+// export const GET_PROFILE_API = "http://localhost:3000/profile";
+
+const BASE_URL = "https://intern-api-ew1g.onrender.com";
+
+// auth APIs
+export const LOGIN_API = `${BASE_URL}/login`;
+export const SIGNUP_API = `${BASE_URL}/signup`;
+export const LOGOUT_API = `${BASE_URL}/logout`;
 
 // Profile
-export const GET_PROFILE_API = "http://localhost:3000/profile";
+export const GET_PROFILE_API = `${BASE_URL}/profile`;

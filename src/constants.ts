c1129b9e969874b230d915034df9885b0b2ea99e
rajasekhargeddam <ganeshrajasekhar219@gmail.com>
2026-08-18 11:33:32 +0530

    made BASE_URL dynamic

--- src/constants.ts
+++ src/constants.ts
@@ -6,10 +6,13 @@ export const POSTS_PER_PAGE = 12;
 // export const BASE_URL = "https://intern-api-ew1g.onrender.com";
 
 // localhost
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
 
 // aws
 // export const BASE_URL = "/api";
+
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
 
 export const api_status = {
   loading: "LOADING",
